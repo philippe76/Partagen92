1409fe055ee71518c2d3c52adbae67f1e0264fe6
Philippe Lanougadere <soufiane92230@gmail.com>
2021-07-07 17:54:54 +0200

    Update form contact style

--- form.php
+++ form.php
@@ -1,26 +1,26 @@
 <?php
+
 // variables form data 
-  $last_name = $_POST['last_name'];
-  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last-name'];
+  $first_name = $_POST['first-name'];
   $mail = $_POST['mail'];
   $commentaire = $_POST['commentaire'];
 
 // variables for mailing form data  
-  $website = "partagen92.fr"
-  $email_subject = "commentaire visiteur";
-  $email_body = "Commentaire de $first_name $last_name. \n".
-                "Voici le commentaire:\n $commentaire "
+  $website = "partagen92.fr";
+  $email_subject = "Commentaire visiteur partagen92.fr";
+  $email_body = "Auteur : $first_name $last_name. \n".
+                "Adresse mail : $mail. \n".
+                "Commentaire :\n $commentaire";
 
   $to = "p.lanougadere@gmail.com";
   $headers = "From: $website \r\n";
 
-
 // mailing form data
-  mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers);
 
 // redirecting
-    header("contact.html")
-
+// header("Location: http://127.0.0.1:5500/index.html");
+header("Location: http://127.0.0.1:5500/test.html");
 
 ?>
-
